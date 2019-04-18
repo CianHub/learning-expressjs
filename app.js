beb57bc7b6ab882f572d86f0e9a4fc6da8731d71
Cian O'Gralaigh <ihatecian@gmail.com>
2019-04-18 15:17:30 +0100

    add path

--- app.js
+++ app.js
@@ -7,20 +7,20 @@ const express = require('express');
 // Creates an express object
 const app = express();
 
-// Allows for adding middleware
-app.use((req, res, next) => {
+//Allows for adding middleware
+app.use('/test-path', (req, res, next) => {
   console.log(app);
   next();
 });
 
 // Next allows us to get to the next middleware
 // res.send allows us to send a response and attach a body of type any
-app.use((req, res, next) => {
+// '/' is the default path if not specified
+app.use('/', (req, res, next) => {
   console.log('Thats an express object');
   res.send('<h1>yo</h1>');
 });
 
 // An express object is a valid requestHandler
-const server = http.createServer(app);
-
-server.listen(3000);
+// Listen method combines nodes http create server and listen methods into one
+app.listen(3000);
